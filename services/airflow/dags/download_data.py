1c5ba70e563dc6628c21a7b5e449e207a819ba63
edarchimbaud <edouard.darchimbaud@gmail.com>
2022-04-19 22:12:56 +0200

    fix: bug fixing

--- services/airflow/dags/download_data.py
+++ services/airflow/dags/download_data.py
@@ -23,14 +23,12 @@ class Client:
         )
         response_json = response.json()
         error = response_json["error"]
-        if error is not None:
-            print(error)
         data = response_json["data"]
         if data is None:
-            return
+            return None, error
         dfm = pd.DataFrame.from_dict(data)
         dfm = dfm.set_index(["Date", "Stem"])
-        return dfm
+        return dfm, error
 
     def get_daily_ohlcv(self, ric, start_date, end_date):
         response = requests.get(
@@ -44,14 +42,12 @@ class Client:
         )
         response_json = response.json()
         error = response_json["error"]
-        if error is not None:
-            print(error)
         data = response_json["data"]
         if data is None:
-            return
+            return None, error
         dfm = pd.DataFrame.from_dict(data)
         dfm = dfm.set_index(["Date", "RIC"])
-        return dfm
+        return dfm, error
 
     def get_expiry_calendar(self, ticker, start_date, end_date):
         response = requests.get(
@@ -65,18 +61,18 @@ class Client:
         )
         response_json = response.json()
         error = response_json["error"]
-        if error is not None:
-            print(error)
         data = response_json["data"]
         if data is None:
-            return
+            return None, error
         dfm = pd.DataFrame.from_dict(data)
-        return dfm
+        return dfm, error
 
     def get_tickers(self):
         response = requests.get(f"http://localhost:8000/tickers", headers=self.headers)
-        data = response.json().get("data", [])
-        return data
+        response_json = response.json()
+        data = response_json.get("data", [])
+        error = response_json["error"]
+        return data, error
 
 
 @click.command()
@@ -87,7 +83,8 @@ def main(from_ticker, to_ticker, with_expiry):
     client = Client()
     start_date = "1990-01-01"
     end_date = "2022-02-28"
-    for ticker, future in tqdm(client.get_tickers().items()):
+    tickers, _ = client.get_tickers()
+    for ticker, future in tqdm(tickers.items()):
         if (from_ticker != "" and ticker < from_ticker) or (
             to_ticker != "" and ticker > to_ticker
         ):
@@ -97,7 +94,7 @@ def main(from_ticker, to_ticker, with_expiry):
         carry_factor = future.get("CarryFactor", {})
         group = future.get("Group")
         if with_expiry:
-            dfm = client.get_expiry_calendar(
+            dfm, _ = client.get_expiry_calendar(
                 ticker, start_date="1990-01-01", end_date=date.today().isoformat()
             )
             path = os.path.join(
@@ -106,45 +103,49 @@ def main(from_ticker, to_ticker, with_expiry):
             dfm.to_csv(path, index=False)
         else:
             if group == "Commodities":
-                dfm = client.get_daily_factor(
+                dfm, _ = client.get_daily_factor(
                     "carry/commodity", ticker, start_date, end_date
                 )
                 print(dfm)
             if group == "Currencies":
                 if "LocalInterestRate" in carry_factor:
-                    dfm = client.get_daily_factor(
+                    dfm, _ = client.get_daily_factor(
                         "carry/currency", ticker, start_date, end_date
                     )
                     print(dfm)
             if group == "Equities":
                 if "ExpectedDividend" in carry_factor:
-                    dfm = client.get_daily_factor(
+                    dfm, _ = client.get_daily_factor(
                         "carry/equity", ticker, start_date, end_date
                     )
                     print(dfm)
             if group == "Rates":
                 if "GovernmentInterestRate5Y" in carry_factor:
-                    dfm = client.get_daily_factor(
+                    dfm, _ = client.get_daily_factor(
                         "carry/bond", ticker, start_date, end_date
                     )
                     print(dfm)
             if "COT" in future:
-                dfm = client.get_daily_factor("cot", ticker, start_date, end_date)
+                dfm, _ = client.get_daily_factor("cot", ticker, start_date, end_date)
                 print(dfm)
             if "CurrencyFactor" in future:
-                dfm = client.get_daily_factor("currency", ticker, start_date, end_date)
+                dfm, _ = client.get_daily_factor(
+                    "currency", ticker, start_date, end_date
+                )
                 print(dfm)
-            dfm = client.get_daily_factor("roll-return", ticker, start_date, end_date)
+            dfm, _ = client.get_daily_factor(
+                "roll-return", ticker, start_date, end_date
+            )
             print(dfm)
-            dfm = client.get_daily_factor("nav/long", ticker, start_date, end_date)
+            dfm, _ = client.get_daily_factor("nav/long", ticker, start_date, end_date)
             print(dfm)
-            dfm = client.get_daily_factor("nav/short", ticker, start_date, end_date)
+            dfm, _ = client.get_daily_factor("nav/short", ticker, start_date, end_date)
             print(dfm)
             stem = future["Stem"]["Reuters"]
             ric = f"{stem}c1"
-            dfm = client.get_daily_ohlcv(ric, start_date, end_date)
+            dfm, _ = client.get_daily_ohlcv(ric, start_date, end_date)
             print(dfm)
-            dfm = client.get_daily_factor("splits", ticker, start_date, end_date)
+            dfm, _ = client.get_daily_factor("splits", ticker, start_date, end_date)
             print(dfm)
 
 
